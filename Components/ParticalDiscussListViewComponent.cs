a10f36a98ade126b7aa337f3ddafee07e950ccca
Boris <AlabamaBleach@gmail.com>
2021-07-14 14:29:21 +0300

    added discuss, leaderboard, solve&challenges, home pages
    1. leaderboard
    2. discusses
    3. solve challenge
    4. challenges
    5. home

---
+++ Components/ParticalDiscussListViewComponent.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Project.Services;
+using Project.Models;
+
+namespace Project.Components
+{
+    public class ParticalDiscussListViewComponent : ViewComponent
+    {
+        private DiscussService _discussService;
+        public ParticalDiscussListViewComponent(DiscussService serv)
+        {
+            _discussService = serv;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            Discuss[] getAllDiscuss = _discussService.GetAllDiscusses().ToArray();
+
+            //List<Discuss> mostPopular = getAllDiscuss.OrderBy(x => x.Li)
+
+            List<Discuss> buildTenDiscusses = new List<Discuss>();
+            var endOf = getAllDiscuss.ToArray().Length >= 10 ? 10 : getAllDiscuss.ToArray().Length;
+            for (int i = 0; i < endOf; i++)
+            {
+                buildTenDiscusses.Add(getAllDiscuss[i]);
+            }
+
+            return View(buildTenDiscusses);
+        }
+    }
+}
