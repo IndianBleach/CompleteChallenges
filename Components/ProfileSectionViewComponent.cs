d28cbb98eec3e269a8897d24538630646401cbf6
Boris <AlabamaBleach@gmail.com>
2021-07-25 18:04:38 +0300

    update frontend pages
    one of some last frontend update

---
+++ Components/ProfileSectionViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Project.Models;
+
+namespace Project.Components
+{
+    public class ProfileSectionViewComponent : ViewComponent
+    {
+        public ProfileSectionViewComponent()
+        {
+        }
+
+        public IViewComponentResult Invoke(ViewSection section)
+        {
+            if (string.IsNullOrEmpty(section.Name))
+            {
+                return View("activity");
+            }
+            else
+            {
+                return View(section.Name);
+            }
+        }        
+    }
+}
